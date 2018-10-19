7953be8f0273ffaa62c6c66cfe91543bfcadf12f
Piotr Pasich <piotr.pasich@gmail.com>
2018-10-19 09:00:29 +0200

    food

--- src/food.js
+++ src/food.js
@@ -1,6 +1,6 @@
 function Food() {
   this.x = floor(random(windowWidth/rectangleSize))*rectangleSize
-  this.y = rectangleSize;//floor(random(windowHeight/rectangleSize))*rectangleSize
+  this.y = floor(random(windowHeight/rectangleSize))*rectangleSize
 
   this.show = function() {
     fill(255)
